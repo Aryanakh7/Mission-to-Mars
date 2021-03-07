90809f08b4ce4263d11ec9cf22e4fda57e1a92f5
Aryanakh7 <Aryanakh7@users.noreply.github.com>
2021-03-07 17:07:48 -0500

    Final Update

---
+++ Mission_to_Mars_Challenge.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+
+# In[3]:
+
+
+# Set the executable path and initialize the chrome browser in splinter
+from webdriver_manager.chrome import ChromeDriverManager
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[4]:
+
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+
+# In[5]:
+
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+
+# In[6]:
+
+
+slide_elem.find("div", class_='content_title')
+
+
+# In[7]:
+
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+
+# In[8]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+# In[9]:
+
+
+# Visit URL
+url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+browser.visit(url)
+
+
+# In[10]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[11]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[12]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[13]:
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+img_url
+
+
+# In[14]:
+
+
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+
+# In[15]:
+
+
+df.to_html()
+
+
+# In[16]:
+
+
+# browser.quit()
+
+
+# In[17]:
+
+
+# D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+
+
+# In[18]:
+
+
+### Hemispheres
+
+
+# In[76]:
+
+
+# 1. Use browser to visit the URL 
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(url)
+
+
+# In[73]:
+
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+for i in range(4):
+    hemisphere = {}
+    
+    browser.find_by_css('a.product-item h3')[i].click()
+    
+    image_elem = browser.links.find_by_text('Sample').first
+    
+   
+    hemisphere['title'] = browser.find_by_css("h2.title").text
+    img_url = image_elem['href']
+    hemisphere['img_url'] = img_url
+   
+   
+    hemisphere_image_urls.append(hemisphere)
+    
+    browser.back()
+    
+    
+
+
+# In[74]:
+
+
+# 4. Print the list that holds the dictionary of each image url and title.
+hemisphere_image_urls
+
+
+# In[75]:
+
+
+# 5. Quit the browser
+browser.quit()
+
+
+# In[ ]:
+
+
+
+
